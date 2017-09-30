6ad9c7dc60658de8fcb72697140636d7a8ea1e4e
mattbrailsford <me@mattbrailsford.com>
2017-09-30 22:38:21 +0100

    Sticky sidebar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+// Detect IE version
+var ie = (function () {
+
+    var undef,
+        v = 3,
+        div = document.createElement('div'),
+        all = div.getElementsByTagName('i');
+
+    while (
+        div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+        all[0]
+    );
+
+    return v > 4 ? v : undef;
+
+}());
+
 !(function ($, undefined) {
 
     var initTablet = function () {
@@ -61,6 +78,17 @@
             'resizeDuration': 200,
             'maxWidth': 1000
         });
+
+        // Init common sticky elements
+        if (!ie || ie > 9)
+        {
+            $(".sticky").each(function (idx, itm) {
+                $(itm).stick_in_parent({
+                    offset_top: 30,
+                    inner_scrolling: true
+                });
+            });
+        }
 
         /*
         $(".carousel").cycle({
